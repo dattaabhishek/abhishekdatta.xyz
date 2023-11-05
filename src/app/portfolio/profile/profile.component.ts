11fa750f23d0ae0a7636588f2bec86605c9d1c4e
Abhishek Datta <53404564+dattaabhishek@users.noreply.github.com>
2023-11-05 22:54:49 +0530

    link issue fix 3

--- src/app/portfolio/profile/profile.component.ts
+++ src/app/portfolio/profile/profile.component.ts
@@ -14,7 +14,7 @@ export class ProfileComponent {
   ]);
   // array
   aboutProjects = [
-    `A guitar enthusiast who likes to share his knowledge with others lorem ipsum aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`,
+    `A guitar enthusiast who likes to share his knowledge with others`,
     `Goodman Agency is a gym-marketing agency.
     I've helped them transform their unfinished website into a fully working markatable web-application ready to attract potential customers.`,
     `E-learning platform`
