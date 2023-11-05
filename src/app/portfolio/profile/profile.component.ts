fcad9e4402299be7c72ff8b9406f970d281b2c5f
Abhishek Datta <53404564+dattaabhishek@users.noreply.github.com>
2023-11-05 22:42:32 +0530

    added hyperlinks to projects

--- src/app/portfolio/profile/profile.component.ts
+++ src/app/portfolio/profile/profile.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.sass']
 })
 export class ProfileComponent {
+  // hashmap
+  projects= new Map([
+    ["Honeybee Tetranine","https://www.google.com/"],
+    ["Goodman Agency","https://goodmanagency.co/"],
+    ["LFF101","not hosted"]
+  ]);
+  // array
+  aboutProjects = [
+    `A guitar enthusiast who likes to share his knowledge with others lorem ipsum aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`,
+    `Goodman Agency is a gym-marketing agency.
+    I've helped them transform their unfinished website into a fully working markatable web-application ready to attract potential customers.`,
+    `E-learning platform`
+  ]
 
 }
