90e1fc368a8e81d2a5033158fc632710cacfa3c2
Abhishek Datta <53404564+dattaabhishek@users.noreply.github.com>
2023-11-08 09:21:54 +0530

    minor changes

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BannerComponent } from './banner/banner.component';
 import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
   declarations: [
-    BannerComponent,
     ProfileComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    BannerComponent,
     ProfileComponent
   ]
 })
